335d063345affd6af384012885f0e4088315f975
Edward Aziz <edazizovv@gmail.com>
2020-06-09 21:44:30 +0300

    Modelling: III

--- m_utils/feature_selection.py
+++ m_utils/feature_selection.py
@@ -14,18 +14,20 @@ def recursive_batch(model, model_params, X, Y, censor, X_test=None, Y_test=None,
     XX, YY = X.copy(), Y.copy()
     mask = numpy.array([True] * XX.shape[1])
     while not done:
-        XX, YY = XX[mask], YY
+        XX, YY = XX[:, mask], YY
+        model_ = model(**model_params)
+        model_.fit(XX, YY)
         if names:
             XX_ = XX_[mask]
         if test:
-            XX_test, YY_test = XX_test[mask], YY_test
-        mask = censor(model, XX, YY)
-        done = mask.sum() == 0
+            XX_test, YY_test = XX_test[:, mask], YY_test
+        mask = censor(model_, XX, YY)
+        done = mask.sum() == 0 or mask.all() == 1
     if not names and not test:
-        return XX, YY
+        return model_, XX, YY
     if not names and test:
-        return XX, YY, XX_test, YY_test
+        return model_, XX, YY, XX_test, YY_test
     if names and not test:
-        return XX, YY, XX_
+        return model_, XX, YY, XX_
     if names and test:
-        return XX, YY, XX_test, YY_test, XX_
+        return model_, XX, YY, XX_test, YY_test, XX_
